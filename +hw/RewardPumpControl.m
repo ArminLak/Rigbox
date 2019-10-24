2326991d76e6f74c47a4470b819dbe220499a307
Peter Zatka-Haas <peter@peterzh.com>
2019-10-24 15:54:13 +0100

    Higher default reward

--- +hw/RewardPumpControl.m
+++ +hw/RewardPumpControl.m
@@ -14,7 +14,7 @@ classdef RewardPumpControl < hw.ControlSignalGenerator & handle
   methods
       function obj = RewardPumpControl()
           obj.DefaultValue = 0;
-          obj.DefaultCommand = obj.microLitresPerMicroStep*3;
+          obj.DefaultCommand = obj.microLitresPerMicroStep*15;
       end
     
       function samples = waveform(obj, ~, microLitres)
