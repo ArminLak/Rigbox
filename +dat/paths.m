9847fe5345cf85b896f62fabf4b2479ef36706e9
Peter Zatka-Haas <peter@peterzh.com>
2019-11-12 13:26:06 +0000

    Updated paths file for lab and enforced in repository

---
+++ +dat/paths.m
@@ -0,0 +1,61 @@
+function p = paths(rig)
+%DAT.PATHS Returns struct containing important paths
+%   p = DAT.PATHS([RIG]) Returns a struct of paths that are used by Rigbox
+%   to determine the location of config and experiment files.  The rig
+%   input is used to generate rig specific paths, including custom paths.
+%   The default is the hostname of this computer.
+%
+%   The main and local repositories are essential for determining where to
+%   save experimental data.
+%
+% Part of Rigbox
+
+% 2013-03 CB created
+% 2019-11 PZH updated for Lak Lab
+
+thishost = hostname;
+
+if nargin < 1 || isempty(rig)
+  rig = thishost;
+end
+
+server1Name = '\\WIN-AL003\B_SERVER';
+
+%% Essential paths
+% Path containing rigbox config folders
+p.rigbox = fileparts(which('addRigboxPaths'));
+% Repository for local copy of everything generated on this rig
+p.localRepository = 'C:\LocalExpData';
+
+% Data are grouped by mouse name
+p.mainRepository = fullfile(server1Name, 'Data', 'Subjects');
+
+% Directory for organisation-wide configuration files
+p.globalConfig = fullfile(server1Name, 'Code', 'Rigging', 'config');
+% Directory for rig-specific configuration files
+p.rigConfig = fullfile(p.globalConfig, rig);
+% Repository for all experiment definitions
+p.expDefinitions = fullfile(server1Name, 'Code', 'Rigging', 'ExpDefinitions');
+
+%% Non-essential paths
+% Location of git for automatic updates
+p.gitExe = 'C:\Program Files\Git\cmd\git.exe'; 
+% Day on which to update code (0 = Everyday, 1 = Sunday, etc.)
+p.updateSchedule = 0;
+
+
+%% user-defined repositories
+% The following paths are not used in the main Rigbox code, however may be
+% added to this file when using the +dat package for one's own analysis.
+% Some examples below:
+
+% Repository for working analyses that are not meant to be stored
+% permanently
+% p.workingAnalysisRepository = fullfile(basketName, 'data');
+% For tape backups, first files go here:
+% p.tapeStagingRepository = fullfile(lugaroName, 'bigdrive', 'staging'); 
+% Then they go here:
+% p.tapeArchiveRepository = fullfile(lugaroName, 'bigdrive', 'toarchive');
+
+
+end
